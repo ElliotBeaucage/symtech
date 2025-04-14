332cdd5354ac594bef735d2a7c03ad94f138c13c
Elliot <elliotbeaucage2004@gmail.com>
2025-04-14 11:20:42 -0400

    Ajout de la relations appartien a client

--- app/Models/Building.php
+++ app/Models/Building.php
@@ -4,11 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Building extends Model    
+class Building extends Model
 {
     //
     public function machine() {
         return $this->hasMany(Machine::class);
     }
-    
+    public function client()
+{
+    return $this->belongsTo(Client::class);
+}
+
 }
