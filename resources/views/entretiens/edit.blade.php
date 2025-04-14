705ef8f54153f62c79718ce5544550d9b15ea70a
Elliot <elliotbeaucage2004@gmail.com>
2025-04-14 12:33:04 -0400

    permet au telephone d'ajouter des img

--- resources/views/entretiens/edit.blade.php
+++ resources/views/entretiens/edit.blade.php
@@ -50,7 +50,7 @@
                         <label for="images" class="block text-sm font-medium text-gray-700 mb-1">Ajouter des
                             photos</label>
                         <input type="file" name="images[]" multiple
-                            class="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-[#003E7E]">
+                            class="w-full border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-[#003E7E]" accept="image/*" capture="environment">
                     </div>
 
                     {{-- Affichage des images existantes --}}
