fdb424c49bb7b67ba7c65f7f0b5ae2da48d71703
ElliotBeaucage <elliotbeaucage2004@gmail.com>
2025-05-27 21:52:39 +0000

    Mise à jour Symtech : manifest + icônes + PWA

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,7 +8,26 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/signature_pad@4.1.6/dist/signature_pad.umd.min.js"></script>
     <script src="//unpkg.com/alpinejs" defer></script>
+    <link rel="manifest" href="/manifest.json">
+    <link rel="apple-touch-icon" href="/images/icons/icon-192x192.png">
+    <meta name="theme-color" content="#1F2937">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="default">
     <title>Symtech</title>
+ <script>
+      if ("serviceWorker" in navigator) {
+        window.addEventListener("load", function () {
+          navigator.serviceWorker.register("/service-worker.js").then(
+            function (registration) {
+              console.log("ServiceWorker registration successful");
+            },
+            function (err) {
+              console.log("ServiceWorker registration failed: ", err);
+            }
+          );
+        });
+      }
+    </script>
 </head>
 
 <header>
@@ -23,5 +42,7 @@
 </header>
 <body>
     {{{$slot}}}
+
+
 </body>
 </html>
