706c211ceef94d313e9d54f8ee96e88a83b9a885
Elliot <elliotbeaucage2004@gmail.com>
2025-06-25 17:28:00 -0400

    Icone

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,20 +1,24 @@
-
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Librairies -->
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/signature_pad@4.1.6/dist/signature_pad.umd.min.js"></script>
     <script src="//unpkg.com/alpinejs" defer></script>
+
+    <!-- PWA -->
     <link rel="manifest" href="/manifest.json">
-    <link rel="apple-touch-icon" href="/images/icons/icon-192x192.png">
-    <meta name="theme-color" content="#1F2937">
-<meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="apple-mobile-web-app-status-bar-style" content="default">
+    <link rel="apple-touch-icon" href="/icons/symtech_icon_192x192.png">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+
     <title>Symtech</title>
- <script>
+
+    <!-- Service Worker -->
+    <script>
       if ("serviceWorker" in navigator) {
         window.addEventListener("load", function () {
           navigator.serviceWorker.register("/service-worker.js").then(
@@ -29,16 +33,3 @@
       }
     </script>
 </head>
-
-<header class="bg-white-100">
-    <a href="{{route("clients.index")}}">
-        <img src="{{ asset('img/symtec_logo.svg') }}" alt="" width="200px" height="200px">
-    </a>
-</header>
-
-<body class=" bg-gray-100">
-    {{{$slot}}}
-
-
-</body>
-</html>
