65afb689b1a31417f64b4f221f7382a0c621a689
ElliotBeaucage <elliotbeaucage2004@gmail.com>
2025-06-25 21:33:39 +0000

    testtest

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,4 +1,5 @@
 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,33 +8,36 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <!-- Librairies -->
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script src="https://cdn.jsdelivr.net/npm/signature_pad@4.1.6/dist/signature_pad.umd.min.js"></script>
-    <script src="//unpkg.com/alpinejs" defer></script>
+    <script src="h
+    <title>Symtech</title>
 
-    <!-- PWA -->
+    <!-- PWA Meta -->
     <link rel="manifest" href="/manifest.json">
-    <link rel="apple-touch-icon" href="/icons/symtech_icon_192x192.png">
-    <meta name="theme-color" content="#ffffff">
+    <link rel="apple-touch-icon" href="/img/icons/icon-192x192.png">
+    <meta name="theme-color" content="#1F2937">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="default">
 
-    <title>Symtech</title>
+    <!-- CSS & JS CDN -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.jsdelivr.net/npm/signature_pad@4.1.6/dist/signature_pad.umd.min.js"></script>
+    <script src="https://unpkg.com/alpinejs" defer></script>
 
-    <!-- Service Worker -->
+    <!-- Service Worker Registration -->
     <script>
-      if ("serviceWorker" in navigator) {
-        window.addEventListener("load", function () {
-          navigator.serviceWorker.register("/service-worker.js").then(
-            function (registration) {
-              console.log("ServiceWorker registration successful");
-            },
-            function (err) {
-              console.log("ServiceWorker registration failed: ", err);
-            }
-          );
-        });
-      }
+        if ("serviceWorker" in navigator) {
+            window.addEventListener("load", function () {
+                navigator.serviceWorker.register("/service-worker.js").then(
+                    function (registration) {
+                        console.log("ServiceWorker registration successful");
+                    },
+                    function (err) {
+                        console.log("ServiceWorker registration failed: ", err);
+                    }
+                );
+            });
+        }
+
     </script>
 </head>
 
