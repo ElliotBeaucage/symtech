0a681b29b9042af36f87f04816de6ccb53e2a55f
Elliot <elliotbeaucage2004@gmail.com>
2025-06-25 17:38:49 -0400

    please work

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,4 @@
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,46 +7,34 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <!-- Librairies -->
-    <script src="h
-    <title>Symtech</title>
-
-    <!-- PWA Meta -->
-    <link rel="manifest" href="/manifest.json">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="shortcut icon" href="/apple-touch-icon.png">
-    <meta name="theme-color" content="#1F2937">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="default">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <!-- CSS & JS CDN -->
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/signature_pad@4.1.6/dist/signature_pad.umd.min.js"></script>
-    <script src="https://unpkg.com/alpinejs" defer></script>
+    <script src="//unpkg.com/alpinejs" defer></script>
 
-    <!-- Service Worker Registration -->
+    <!-- PWA -->
+    <link rel="manifest" href="/manifest.json">
+    <link rel="apple-touch-icon" href="/icons/symtech_icon_192x192.png">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+
+    <title>Symtech</title>
+
+    <!-- Service Worker -->
     <script>
-        if ("serviceWorker" in navigator) {
-            window.addEventListener("load", function () {
-                navigator.serviceWorker.register("/service-worker.js").then(
-                    function (registration) {
-                        console.log("ServiceWorker registration successful");
-                    },
-                    function (err) {
-                        console.log("ServiceWorker registration failed: ", err);
-                    }
-                );
-            });
-        }
-
+      if ("serviceWorker" in navigator) {
+        window.addEventListener("load", function () {
+          navigator.serviceWorker.register("/service-worker.js").then(
+            function (registration) {
+              console.log("ServiceWorker registration successful");
+            },
+            function (err) {
+              console.log("ServiceWorker registration failed: ", err);
+            }
+          );
+        });
+      }
     </script>
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-C6KWCH7R0M"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-C6KWCH7R0M');
-</script>
 </head>
 
 
