b63944fde97531cde159f0bda0d0083191f15e4c
Elliot <elliotbeaucage2004@gmail.com>
2025-06-25 17:41:32 -0400

    offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,34 @@
-self.addEventListener("install", function (e) {
-  console.log("Service Worker installed");
+const CACHE_NAME = "symtech-cache-v1";
+const urlsToCache = [
+  "/",
+  "/offline.html",
+  "/icons/symtech_icon_192x192.png",
+  "/icons/symtech_icon_512x512.png"
+];
+
+// Installation
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener("fetch", function (event) {
-  event.respondWith(fetch(event.request));
+// Activation
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request).then(response => response || caches.match("/offline.html")))
+  );
 });
